ea23cd1125de4838bf97f5e3c07eb2498b883f44
Rattanawadi Niothong <deuanz@RattanawadisMBP.hkw.proteus-tech.com>
2018-03-21 23:07:15 +0700

    fix test and edit readme

--- aiohttp/controller.py
+++ aiohttp/controller.py
@@ -2,6 +2,6 @@ from aiohttp import web
 
 async def handle(request):
     name = request.match_info.get('name', "World!")
-    text = "Hello  " + name
+    text = "Hello " + name
     print('received request, replying with "{}".'.format(text))
     return web.Response(text=text)
--- aiohttp/test/test_base.py
+++ aiohttp/test/test_base.py
@@ -2,26 +2,26 @@ from aiohttp.test_utils import TestClient, loop_context
 from aiohttp import web, ClientSession
 import asyncio
 
-HOST='http://localhost:7000'
+HOST='http://localhost:8081'
 
 
 loop = asyncio.get_event_loop()
 async def test_case_001():
     async with ClientSession() as session:
-        resp = await session.get('{}/123124'.format(HOST))
+        resp = await session.get('{}/hotnow'.format(HOST))
         assert resp.status == 200
         text = await resp.text()
-        assert 'Hello--------------, 123124' in text
+        assert 'Hello hotnow' in text
         print('test complete')
 loop.run_until_complete(test_case_001())
 
 
 async def test_case_002():
     async with ClientSession() as session:
-        resp = await session.get('{}/123124'.format(HOST))
+        resp = await session.get('{}/hotnow'.format(HOST))
         assert resp.status == 200
         text = await resp.text()
-        assert 'Hello--------------, 123124' in text
+        assert 'Hello hotnow' in text
         print('test complete')
 loop.run_until_complete(test_case_002())
 
