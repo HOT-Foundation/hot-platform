ae714584910c6aecd4bdf862c4d350e37e3d4685
Ben Scherrey <scherrey@proteus-tech.com>
2018-04-04 00:26:01 +0700

    coverage for all source.

--- runtests.sh
+++ runtests.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker-compose run --rm hotnow-token-platform pytest -v --cov-report html --cov-report term-missing --cov=account
+docker-compose run --rm hotnow-token-platform pytest -v --cov-report html --cov-report term-missing  --cov=.
