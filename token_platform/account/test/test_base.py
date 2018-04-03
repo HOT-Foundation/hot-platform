53d4a784f03e9590d49fa3bad4c4c73a28f9aebe
Rattanawadi <rattanawadi@proteus-tech.com>
2018-04-03 15:51:22 +0700

    test via mock request GET: account/{account_address} and mock address

--- token_platform/account/test/test_base.py
+++ token_platform/account/test/test_base.py
@@ -2,62 +2,119 @@ import pytest
 from aiohttp import web
 import asyncio
 from router import routes
-
-from unittest.mock import patch
+from asynctest import patch
 from account.get_account import get_account, Address
 from utils.nametuple_mapping import map_namedtuple
+from aiohttp.test_utils import make_mocked_request
+import json
 
-async def xtest_call_get_account(cli):
-    with patch.object(Address, 'get') as mock_address:
-        mock_data = {
-            'address': 'GBVJJJH6VS5NNM5B4FZ3JQHWN6ANEAOSCEU4STPXPB24BHD5JO5VTGAD',
-            'sequence': '33880235334172680',
-            'balances': [
-                {
-                    'balance': '7.0000000',
-                    'limit': '922337203685.4775807',
-                    'asset_type': 'credit_alphanum4',
-                    'asset_code': 'RNTK',
-                    'asset_issuer': 'GAKGRSAWXQBPU4GNGHUBFV5QNKMN5BDJ7AA5DNHLZGQG6VPO52WU5TQD'
-                },
-                {
-                    'balance': '9.9999200',
-                    'asset_type': 'native'
-                }
-            ],
-            'paging_token': '',
-            'thresholds': {
-                'low_threshold': 1,
-                'med_threshold': 2,
-                'high_threshold': 2
+@asyncio.coroutine
+@patch('account.get_account.Address')
+async def test_call_get_account_success(mock_address):
+    instance = mock_address.return_value
+    mock_data = {
+        'address': 'GBVJJJH6VS5NNM5B4FZ3JQHWN6ANEAOSCEU4STPXPB24BHD5JO5VTGAD',
+        'sequence': '33880235334172680',
+        'balances': [
+            {
+                'balance': '7.0000000',
+                'limit': '922337203685.4775807',
+                'asset_type': 'credit_alphanum4',
+                'asset_code': 'RNTK',
+                'asset_issuer': 'GAKGRSAWXQBPU4GNGHUBFV5QNKMN5BDJ7AA5DNHLZGQG6VPO52WU5TQD'
             },
-            'signers': [
-                {
-                    'public_key': 'GDBNKZDZMEKXOH3HLWLKFMM7ARN2XVPHWZ7DWBBEV3UXTIGXBTRGJLHF',
-                    'weight': 1,
-                    'key': 'GDBNKZDZMEKXOH3HLWLKFMM7ARN2XVPHWZ7DWBBEV3UXTIGXBTRGJLHF',
-                    'type': 'ed25519_public_key'
-                },
-                {
-                    'public_key': 'GDHH7XOUKIWA2NTMGBRD3P245P7SV2DAANU2RIONBAH6DGDLR5WISZZI',
-                    'weight': 1,
-                    'key': 'GDHH7XOUKIWA2NTMGBRD3P245P7SV2DAANU2RIONBAH6DGDLR5WISZZI',
-                    'type': 'ed25519_public_key'
-                },
-                {
-                    'public_key': 'GBVJJJH6VS5NNM5B4FZ3JQHWN6ANEAOSCEU4STPXPB24BHD5JO5VTGAD',
-                    'weight': 0,
-                    'key': 'GBVJJJH6VS5NNM5B4FZ3JQHWN6ANEAOSCEU4STPXPB24BHD5JO5VTGAD',
-                    'type': 'ed25519_public_key'
-                }
-            ],
-            'data': {}
-        }
+            {
+                'balance': '9.9999200',
+                'asset_type': 'native'
+            }
+        ],
+        'paging_token': '',
+        'thresholds': {
+            'low_threshold': 1,
+            'med_threshold': 2,
+            'high_threshold': 2
+        },
+        'signers': [
+            {
+                'public_key': 'GDBNKZDZMEKXOH3HLWLKFMM7ARN2XVPHWZ7DWBBEV3UXTIGXBTRGJLHF',
+                'weight': 1,
+                'key': 'GDBNKZDZMEKXOH3HLWLKFMM7ARN2XVPHWZ7DWBBEV3UXTIGXBTRGJLHF',
+                'type': 'ed25519_public_key'
+            },
+            {
+                'public_key': 'GDHH7XOUKIWA2NTMGBRD3P245P7SV2DAANU2RIONBAH6DGDLR5WISZZI',
+                'weight': 1,
+                'key': 'GDHH7XOUKIWA2NTMGBRD3P245P7SV2DAANU2RIONBAH6DGDLR5WISZZI',
+                'type': 'ed25519_public_key'
+            },
+            {
+                'public_key': 'GBVJJJH6VS5NNM5B4FZ3JQHWN6ANEAOSCEU4STPXPB24BHD5JO5VTGAD',
+                'weight': 0,
+                'key': 'GBVJJJH6VS5NNM5B4FZ3JQHWN6ANEAOSCEU4STPXPB24BHD5JO5VTGAD',
+                'type': 'ed25519_public_key'
+            }
+        ],
+        'data': {}
+    }
+    class MockAddress(object):
+        def get(self):
+            pass
 
-        mock_address.return_value = map_namedtuple(mock_data)
-        result = await cli.get('/account/GBVJJJH6VS5NNM5B4FZ3JQHWN6ANEAOSCEU4STPXPB24BHD5JO5VTGAD')
-        json = await result.json()
-        assert json == {}
+        @property
+        def balances(self):
+            return mock_data['balances']
+
+        @property
+        def signers(self):
+            return mock_data['signers']
+
+        @property
+        def thresholds(self):
+            return mock_data['thresholds']
+
+    mock_address.return_value = MockAddress()
+
+    req = make_mocked_request('GET', '/account/{}'.format('GBVJJJH6VS5NNM5B4FZ3JQHWN6ANEAOSCEU4STPXPB24BHD5JO5VTGAD'),
+        match_info={'account_address': 'GBVJJJH6VS5NNM5B4FZ3JQHWN6ANEAOSCEU4STPXPB24BHD5JO5VTGAD'}
+    )
+
+    result = await get_account(req)
+    actual_data = json.loads(result.text)
+    expect_data = {
+        '@url': 'localhost:8080/account/GBVJJJH6VS5NNM5B4FZ3JQHWN6ANEAOSCEU4STPXPB24BHD5JO5VTGAD',
+        '@id': 'GBVJJJH6VS5NNM5B4FZ3JQHWN6ANEAOSCEU4STPXPB24BHD5JO5VTGAD',
+        'asset': {
+            'RNTK': {
+                'balance': '7.0000000',
+                'issuer': 'GAKGRSAWXQBPU4GNGHUBFV5QNKMN5BDJ7AA5DNHLZGQG6VPO52WU5TQD'
+            },
+            'XLM': {
+                'balance': '9.9999200',
+                'issuer': 'native'
+            }
+        },
+        'thresholds': {
+            'low_threshold': 1,
+            'med_threshold': 2,
+            'high_threshold': 2
+        },
+        'signers': [
+            {
+                'public_key': 'GDBNKZDZMEKXOH3HLWLKFMM7ARN2XVPHWZ7DWBBEV3UXTIGXBTRGJLHF',
+                'weight': 1,
+                'type': 'ed25519_public_key'
+            }, {
+                'public_key': 'GDHH7XOUKIWA2NTMGBRD3P245P7SV2DAANU2RIONBAH6DGDLR5WISZZI',
+                'weight': 1,
+                'type': 'ed25519_public_key'
+            }, {
+                'public_key': 'GBVJJJH6VS5NNM5B4FZ3JQHWN6ANEAOSCEU4STPXPB24BHD5JO5VTGAD',
+                'weight': 0,
+                'type': 'ed25519_public_key'
+            }
+        ]
+    }
+    assert actual_data == expect_data
 
 
 async def test_get_account_success(cli):
