aa80983e8f620788c4b53fecc469e1a7a3621abc
Rattanawadi <rattanawadi@proteus-tech.com>
2018-04-06 22:42:20 +0700

    add test incase if wallet has no htkn balance

--- token_platform/wallet/test/factory/wallet.py
+++ token_platform/wallet/test/factory/wallet.py
@@ -1,31 +1,8 @@
 class StellarWallet(object):
-        def __init__(self):
-            pass
-
-        def get(self):
-            pass
-
-        @property
-        def id(self):
-            return 'GBVJJJH6VS5NNM5B4FZ3JQHWN6ANEAOSCEU4STPXPB24BHD5JO5VTGAD'
-
-        @property
-        def balances(self):
-            return [{
-                'balance': '7.0000000',
-                'limit': '922337203685.4775807',
-                'asset_type': 'credit_alphanum4',
-                'asset_code': 'HTKN',
-                'asset_issuer': 'GAKGRSAWXQBPU4GNGHUBFV5QNKMN5BDJ7AA5DNHLZGQG6VPO52WU5TQD'
-            },
-            {
-                'balance': '9.9999200',
-                'asset_type': 'native'
-            }]
-
-        @property
-        def signers(self):
-            return [{
+        def __init__(self, balances):
+            self.id = 'GBVJJJH6VS5NNM5B4FZ3JQHWN6ANEAOSCEU4STPXPB24BHD5JO5VTGAD'
+            self.balances = balances
+            self.signers = [{
                 'public_key': 'GDBNKZDZMEKXOH3HLWLKFMM7ARN2XVPHWZ7DWBBEV3UXTIGXBTRGJLHF',
                 'weight': 1,
                 'key': 'GDBNKZDZMEKXOH3HLWLKFMM7ARN2XVPHWZ7DWBBEV3UXTIGXBTRGJLHF',
@@ -43,11 +20,58 @@ class StellarWallet(object):
                 'key': 'GBVJJJH6VS5NNM5B4FZ3JQHWN6ANEAOSCEU4STPXPB24BHD5JO5VTGAD',
                 'type': 'ed25519_public_key'
             }]
+            self.thresholds = {
+                'low_threshold': 1,
+                'med_threshold': 2,
+                'high_threshold': 2
+            }
 
-        @property
-        def thresholds(self):
-            return {
-            'low_threshold': 1,
-            'med_threshold': 2,
-            'high_threshold': 2
-        }
+        def get(self):
+            pass
+
+        # @property
+        # def id(self):
+        #     return 'GBVJJJH6VS5NNM5B4FZ3JQHWN6ANEAOSCEU4STPXPB24BHD5JO5VTGAD'
+
+        # @property
+        # def balances(self):
+        #     return [{
+        #         'balance': '7.0000000',
+        #         'limit': '922337203685.4775807',
+        #         'asset_type': 'credit_alphanum4',
+        #         'asset_code': 'HTKN',
+        #         'asset_issuer': 'GAKGRSAWXQBPU4GNGHUBFV5QNKMN5BDJ7AA5DNHLZGQG6VPO52WU5TQD'
+        #     },
+        #     {
+        #         'balance': '9.9999200',
+        #         'asset_type': 'native'
+        #     }]
+
+        # @property
+        # def signers(self):
+        #     return [{
+        #         'public_key': 'GDBNKZDZMEKXOH3HLWLKFMM7ARN2XVPHWZ7DWBBEV3UXTIGXBTRGJLHF',
+        #         'weight': 1,
+        #         'key': 'GDBNKZDZMEKXOH3HLWLKFMM7ARN2XVPHWZ7DWBBEV3UXTIGXBTRGJLHF',
+        #         'type': 'ed25519_public_key'
+        #     },
+        #     {
+        #         'public_key': 'GDHH7XOUKIWA2NTMGBRD3P245P7SV2DAANU2RIONBAH6DGDLR5WISZZI',
+        #         'weight': 1,
+        #         'key': 'GDHH7XOUKIWA2NTMGBRD3P245P7SV2DAANU2RIONBAH6DGDLR5WISZZI',
+        #         'type': 'ed25519_public_key'
+        #     },
+        #     {
+        #         'public_key': 'GBVJJJH6VS5NNM5B4FZ3JQHWN6ANEAOSCEU4STPXPB24BHD5JO5VTGAD',
+        #         'weight': 0,
+        #         'key': 'GBVJJJH6VS5NNM5B4FZ3JQHWN6ANEAOSCEU4STPXPB24BHD5JO5VTGAD',
+        #         'type': 'ed25519_public_key'
+        #     }]
+
+        # @property
+        # def thresholds(self):
+        #     return {
+        #     'low_threshold': 1,
+        #     'med_threshold': 2,
+        #     'high_threshold': 2
+        # }
--- token_platform/wallet/test/test_get_account.py
+++ token_platform/wallet/test/test_get_account.py
@@ -8,6 +8,7 @@ from aiohttp.test_utils import make_mocked_request
 import json
 from stellar_base.utils import AccountNotExistError
 from wallet.test.factory.wallet import StellarWallet
+from conf import settings
 
 @patch('wallet.get_wallet.get_wallet')
 async def test_get_wallet_from_request(mock_get_wallet):
@@ -20,9 +21,20 @@ async def test_get_wallet_from_request(mock_get_wallet):
 
 @asyncio.coroutine
 @patch('wallet.get_wallet.StellarAddress')
-async def test_get_wallet_success(mock_address):
+async def test_get_wallet_success_trusted_htkn(mock_address):
     instance = mock_address.return_value
-    mock_address.return_value = StellarWallet()
+    balances =[{
+        'balance': '7.0000000',
+        'limit': '922337203685.4775807',
+        'asset_type': 'credit_alphanum4',
+        'asset_code': 'HTKN',
+        'asset_issuer': 'GAKGRSAWXQBPU4GNGHUBFV5QNKMN5BDJ7AA5DNHLZGQG6VPO52WU5TQD'
+    },
+    {
+        'balance': '9.9999200',
+        'asset_type': 'native'
+    }]
+    mock_address.return_value = StellarWallet(balances)
 
     result = await get_wallet('GBVJJJH6VS5NNM5B4FZ3JQHWN6ANEAOSCEU4STPXPB24BHD5JO5VTGAD')
 
@@ -34,6 +46,33 @@ async def test_get_wallet_success(mock_address):
         '@url': 'localhost:8081/wallet/GBVJJJH6VS5NNM5B4FZ3JQHWN6ANEAOSCEU4STPXPB24BHD5JO5VTGAD',
         'asset': {
             'HTKN': '7.0000000',
+            'XLM': '9.9999200'
+        }
+    }
+    assert actual_data == expect_data
+
+
+@asyncio.coroutine
+@patch('wallet.get_wallet.StellarAddress')
+async def test_get_wallet_success_not_trust_htkn(mock_address):
+    instance = mock_address.return_value
+    balances =[
+    {
+        'balance': '9.9999200',
+        'asset_type': 'native'
+    }]
+    mock_address.return_value = StellarWallet(balances)
+
+    result = await get_wallet('GBVJJJH6VS5NNM5B4FZ3JQHWN6ANEAOSCEU4STPXPB24BHD5JO5VTGAD')
+
+    assert result.status == 200
+
+    actual_data = json.loads(result.text)
+    expect_data = {
+        '@id': 'GBVJJJH6VS5NNM5B4FZ3JQHWN6ANEAOSCEU4STPXPB24BHD5JO5VTGAD',
+        '@url': 'localhost:8081/wallet/GBVJJJH6VS5NNM5B4FZ3JQHWN6ANEAOSCEU4STPXPB24BHD5JO5VTGAD',
+        'trust': '{}/wallet/{}/transaction/change-trust'.format(settings['HOST'], 'GBVJJJH6VS5NNM5B4FZ3JQHWN6ANEAOSCEU4STPXPB24BHD5JO5VTGAD'),
+        'asset': {
             'XLM': '9.9999200'
         }
     }
