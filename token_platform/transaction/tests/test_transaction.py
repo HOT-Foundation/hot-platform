06242b80272aea85776176f3a3b262604dd5d470
sem <pumidon@proteus-tech.com>
2018-04-17 18:03:10 +0700

    Add check duplicate transaction before submit

--- token_platform/transaction/tests/test_transaction.py
+++ token_platform/transaction/tests/test_transaction.py
@@ -4,15 +4,17 @@ from aiohttp.web_exceptions import HTTPBadRequest
 from asynctest import patch
 from tests.test_utils import BaseTestClass
 
-from transaction.transaction import submit_transaction
+from transaction.transaction import submit_transaction, is_duplicate_transaction
 
 
 class TestSubmitTransactionFromRequest(BaseTestClass):
 
     @unittest_run_loop
+    @patch('transaction.transaction.is_duplicate_transaction')
     @patch('transaction.transaction.submit_transaction')
-    async def test_submit_transaction_from_request_success(self, mock) -> None:
-        mock.return_value = {'status': 200}
+    async def test_submit_transaction_from_request_success(self, mock_tx, mock_dup) -> None:
+        mock_dup.return_value = False
+        mock_tx.return_value = {'status': 200}
         url = f'transaction/transaction-hash'
         resp = await self.client.request("PUT", url, data=b'test data')
         assert resp.status == 200
@@ -25,9 +27,11 @@ class TestSubmitTransactionFromRequest(BaseTestClass):
         assert text == expect
 
     @unittest_run_loop
+    @patch('transaction.transaction.is_duplicate_transaction')
     @patch('transaction.transaction.submit_transaction')
-    async def test_submit_transaction_from_request_error_wrong_parameter(self, mock) -> None:
-        mock.return_value = {'status': 200}
+    async def test_submit_transaction_from_request_error_wrong_parameter(self, mock_tx, mock_dup) -> None:
+        mock_dup.return_value = False
+        mock_tx.return_value = {'status': 200}
         url = f'transaction/transaction-hash'
         resp = await self.client.request("PUT", url)
         assert resp.status == 400
@@ -45,3 +49,16 @@ class TestSubmitTransaction(BaseTestClass):
         with pytest.raises(HTTPBadRequest):
             signed_xdr = b'AAAAACRdQ0bLIMY2WB03xlOGSqWXyb8uFQIxp+QSypNpjsW0AAAAZAB79gEAAAAHAAAAAAAAAAAAAAABAAAAAAAAAAAAAAAAbUsVq2bqEoCf7vIHxSuFBHDqxrZiYvtyqyavFkpA9wQAAAABDzN9gAAAAAAAAAABaY7FtAAAAEB0OSEaK6FTDW3imY9vU/jFe3dDhz0j5/656Kpns/uzBdSMUvl9q8ZNQes7ASTp98hgsxfZBlVnYAr0jzwY05MI'
             result = await submit_transaction(signed_xdr)
+
+
+
+
+class TestDuplicateTransaction(BaseTestClass):
+    @unittest_run_loop
+    async def test_is_duplicate_transaction_success(self) -> None:
+        tx_hash = 'e11b7a3677fdd45c885e8fb49d0079d083ee8a5cab08e32b00126172abb05111'
+        result = await is_duplicate_transaction(tx_hash)
+        assert result == True
+        tx_hash = 'e11b7a3677fdd45c885'
+        result = await is_duplicate_transaction(tx_hash)
+        assert result == False
--- token_platform/transaction/transaction.py
+++ token_platform/transaction/transaction.py
@@ -7,10 +7,14 @@ from stellar_base.transaction_envelope import TransactionEnvelope as Te
 
 from conf import settings
 
+async def is_duplicate_transaction(transaction_hash: str):
+    horizon = horizon_livenet() if settings['STELLAR_NETWORK'] == 'PUBLIC' else horizon_testnet()
+    transaction = horizon.transaction(transaction_hash)
+    id = transaction.get('id')
+    return True if id else False
 
 async def submit_transaction(xdr: bytes) -> Dict[str, str]:
-    horizon = horizon_livenet(
-    ) if settings['STELLAR_NETWORK'] == 'PUBLIC' else horizon_testnet()
+    horizon = horizon_livenet() if settings['STELLAR_NETWORK'] == 'PUBLIC' else horizon_testnet()
     try:
         response = horizon.submit(xdr)
     except Exception as e:
@@ -26,6 +30,10 @@ async def submit_transaction_from_request(request: web_request.Request) -> web_r
     signed_xdr = await request.text()
     tx_hash = request.match_info['transaction_hash']
     result = {'message': 'transaction success.'}
+
+    if await is_duplicate_transaction(tx_hash):
+        return web.json_response({'message': 'Duplicate transaction.'}, status=400)
+
     if not signed_xdr or not tx_hash:
         return web.json_response({'message': 'transaction fail, please check your parameter.'}, status=400)
     response = await submit_transaction(signed_xdr)
