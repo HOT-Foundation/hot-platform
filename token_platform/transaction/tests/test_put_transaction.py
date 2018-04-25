51da7e938c0cc6e04569314631df167a33a9a671
sem <pumidon@proteus-tech.com>
2018-04-25 16:28:36 +0700

    Fix error when submit transaction

--- token_platform/transaction/tests/test_put_transaction.py
+++ token_platform/transaction/tests/test_put_transaction.py
@@ -2,31 +2,34 @@ import pytest
 from aiohttp.test_utils import unittest_run_loop
 from asynctest import patch
 from tests.test_utils import BaseTestClass
-
+from aiohttp import web
 
 class TestSubmitTransactionFromRequest(BaseTestClass):
 
     @unittest_run_loop
+    @patch('transaction.put_transaction.get_transaction')
     @patch('transaction.put_transaction.is_duplicate_transaction')
     @patch('transaction.put_transaction.submit_transaction')
-    async def test_put_transaction_from_request_success(self, mock_tx, mock_dup) -> None:
+    async def test_put_transaction_from_request_success(self, mock_tx, mock_dup, mock_get) -> None:
+        expect = {
+            'message': 'transaction success.'
+        }
+
+        mock_get.return_value = web.json_response(expect)
         mock_dup.return_value = False
         mock_tx.return_value = {'status': 200}
         url = f'transaction/transaction-hash'
         resp = await self.client.request("PUT", url, data=b'test data')
         assert resp.status == 200
         text = await resp.json()
-
-        expect = {
-            'message': 'transaction success.'
-        }
+        mock_get.return_value = expect
 
         assert text == expect
 
     @unittest_run_loop
     @patch('transaction.put_transaction.is_duplicate_transaction')
     @patch('transaction.put_transaction.submit_transaction')
-    async def test_put_transaction_from_request_error_wrong_parameter(self, mock_tx, mock_dup) -> None:
+    async def test_put_transaction_from_request_with_no_xdr(self, mock_tx, mock_dup) -> None:
         mock_dup.return_value = False
         mock_tx.return_value = {'status': 200}
         url = f'transaction/transaction-hash'
@@ -35,6 +38,22 @@ class TestSubmitTransactionFromRequest(BaseTestClass):
         text = await resp.json()
         expect = {
             'error': 'transaction fail, please check your parameter.'
+        }
+
+        assert text == expect
+
+    @unittest_run_loop
+    @patch('transaction.put_transaction.is_duplicate_transaction')
+    @patch('transaction.put_transaction.submit_transaction')
+    async def test_put_transaction_from_request_with_duplicate_transaction(self, mock_tx, mock_dup) -> None:
+        mock_dup.return_value = True
+        mock_tx.return_value = {'status': 200}
+        url = f'transaction/transaction-hash'
+        resp = await self.client.request("PUT", url, data=b'test data')
+        assert resp.status == 400
+        text = await resp.json()
+        expect = {
+            'error': 'Duplicate transaction.'
         }
 
         assert text == expect
