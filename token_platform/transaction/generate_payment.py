32bc5b0aaff0ac28756809626be6dd386ff0b089
Rattanawadi <rattanawadi@proteus-tech.com>
2018-05-16 20:23:10 +0700

    make generate_payment can transfer xlm and htkn

--- token_platform/transaction/generate_payment.py
+++ token_platform/transaction/generate_payment.py
@@ -10,8 +10,6 @@ from conf import settings
 from transaction.transaction import get_signers, get_threshold_weight
 from wallet.wallet import get_wallet
 
-JSONType = Union[str, int, float, bool, None, Dict[str, Any], List[Any]]
-
 
 async def generate_payment_from_request(request: web.Request) -> web.Response:
     """AIOHttp Request unsigned transfer transaction"""
@@ -19,28 +17,30 @@ async def generate_payment_from_request(request: web.Request) -> web.Response:
     body = await request.json()
     source_account = request.match_info.get("wallet_address", "")
     target_address = body['target_address']
-    amount = body['amount']
-    meta = body.get('meta', None)
+    amount_htkn = body.get('amount_htkn')
+    amount_xlm = body.get('amount_xlm')
     sequence_number = body.get('sequence_number', None)
+    meta = body.get('meta', None)
 
     await get_wallet(source_account)
     await get_wallet(target_address)
 
-    result = await generate_payment(source_account, target_address, amount, sequence_number)
+    result = await generate_payment(source_account, target_address, amount_htkn, amount_xlm, sequence_number, meta)
     return web.json_response(result)
 
 
-async def generate_payment(source_address: str, destination: str, amount: int, sequence:int = None, meta:str = None) -> JSONType:
+async def generate_payment(source_address: str, destination: str, amount_htkn: Decimal, amount_xlm:Decimal, sequence:int = None, meta:str = None) -> Dict:
     """Get unsigned transfer transaction and signers
 
         Args:
             source_address: Owner of operation
             destination_address: address of receiveing wallet
-            amount: amount of money that would be transferred
+            amount_htkn: amount of HoToken that would be transferred
+            amount_xlm: amount of XLM that would be transferred
             sequence: sequence number for generate transaction [optional]
             meta: memo text [optional]
     """
-    unsigned_xdr, tx_hash = build_unsigned_transfer(source_address, destination, amount, sequence, meta)
+    unsigned_xdr, tx_hash = build_unsigned_transfer(source_address, destination, amount_htkn, amount_xlm, sequence, meta)
     host: str = settings['HOST']
     result = {
         '@id': source_address,
@@ -53,21 +53,32 @@ async def generate_payment(source_address: str, destination: str, amount: int, s
     return result
 
 
-def build_unsigned_transfer(source_address: str, destination_address: str, amount: Union[int, Decimal], sequence=None, memo_text=None) -> Tuple[str, str]:
+def build_unsigned_transfer(source_address: str, destination_address: str, amount_htkn: Decimal, amount_xlm: Decimal, sequence:int=None, memo_text:str=None) -> Tuple[str, str]:
     """"Build unsigned transfer transaction return unsigned XDR and transaction hash.
 
         Args:
             source_address: Owner of operation
             destination_address: wallet id of new wallet
-            amount: starting balance of new wallet
+            amount_htkn: amount of htkn that would be transfer
+            amount_xlm: amount of xlm that would be transfer
+            sequence: sequence number for generate transaction [optional]
+            meta: memo text [optional]
     """
     builder = Builder(address=source_address, network=settings['STELLAR_NETWORK'], sequence=sequence)
+
+    if(amount_htkn):
+        builder.append_payment_op(
+            destination_address, amount_htkn, asset_type=settings['ASSET_CODE'], asset_issuer=settings['ISSUER'], source=source_address
+        )
+
+    if(amount_xlm):
+        builder.append_payment_op(
+            destination_address, amount_xlm, source=source_address
+        )
 
     if(memo_text):
         builder.add_text_memo(memo_text)
 
-    builder.append_payment_op(destination_address, amount, asset_type=settings['ASSET_CODE'],
-                          asset_issuer=settings['ISSUER'], source=source_address)
     unsigned_xdr = builder.gen_xdr()
     tx_hash = builder.te.hash_meta()
     return unsigned_xdr.decode('utf8'), binascii.hexlify(tx_hash).decode()
