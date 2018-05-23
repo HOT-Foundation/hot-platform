a29d0401e37d12fa9ef1f56bd4d7a2d271bb0bb8
Tle Ekkul <aryuth.ekkul@proteus-tech.com>
2018-05-23 16:08:31 +0700

    change query string from meta to memo

--- token_platform/transaction/tests/test_get_transaction.py
+++ token_platform/transaction/tests/test_get_transaction.py
@@ -9,7 +9,7 @@ class TestGetTransactionHashFromRequest(BaseTestClass):
     async def setUpAsync(self):
         self.address = 'GDBNKZDZMEKXOH3HLWLKFMM7ARN2XVPHWZ7DWBBEV3UXTIGXBTRGJLHF'
         self.transaction_url = reverse('get-transaction-hash', wallet_address=self.address)
-        self.params = {'meta': '1'}
+        self.params = {'memo': '1'}
         self.tx_hash = '4c239561b64f2353819452073f2ec7f62a5ad66f533868f89f7af862584cdee9'
 
     @unittest_run_loop
@@ -33,7 +33,7 @@ class TestGetTransactionHashFromRequest(BaseTestClass):
         assert not resp['transaction_hash']
 
     @unittest_run_loop
-    async def test_get_transaction_hash_form_request_not_found_meta(self):
+    async def test_get_transaction_hash_form_request_not_found_memo(self):
         response = await self.client.get(self.transaction_url)
         assert response.status == 400
         resp = await response.json()
