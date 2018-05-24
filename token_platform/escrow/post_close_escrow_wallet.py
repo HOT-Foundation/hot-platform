b3f3ba18f95c5000881390081d1538aba038e440
Thiraput <thiraput@proteus-tech.com>
2018-05-24 16:30:12 +0700

    Addition operation remove trust lines and data

--- token_platform/escrow/post_close_escrow_wallet.py
+++ token_platform/escrow/post_close_escrow_wallet.py
@@ -32,16 +32,8 @@ async def generate_close_escrow_wallet(escrow_address: str) -> Dict:
         transaction_hash: Transaction hash number for get transaction detail
     '''
     escrow_wallet = await get_escrow_wallet_detail(escrow_address)
-    provider_address = escrow_wallet['data']['provider_address']
-    creator_address = escrow_wallet['data']['creator_address']
-    remain_custom_asset: Decimal = Decimal(escrow_wallet['asset'][settings['ASSET_CODE']])
 
-    unsigned_xdr, tx_hash = await build_generate_close_escrow_wallet_transaction(
-                                                escrow_address=escrow_address,
-                                                provider_address=provider_address,
-                                                creator_address=creator_address,
-                                                remain_custom_asset=remain_custom_asset
-                                            )
+    unsigned_xdr, tx_hash = await build_generate_close_escrow_wallet_transaction(escrow_wallet=escrow_wallet)
 
     return {
         'escrow_address' : escrow_address,
@@ -52,10 +44,7 @@ async def generate_close_escrow_wallet(escrow_address: str) -> Dict:
     }
 
 
-async def build_generate_close_escrow_wallet_transaction(escrow_address: str,
-                                                         provider_address: str,
-                                                         creator_address: str,
-                                                         remain_custom_asset: Decimal) -> Tuple[Any, str]:
+async def build_generate_close_escrow_wallet_transaction(escrow_wallet: Dict) -> Tuple[Any, str]:
     """ Builder transaction close escrow wallet by payment remaining HTKN from escrow_wallet to provider_wallet and merge account from escrow_wallet to creator_wallet, Finally, return xdr and transaction_hash
 
     Args:
@@ -64,13 +53,23 @@ async def build_generate_close_escrow_wallet_transaction(escrow_address: str,
         creator_address: an address of source wallet which is owner of the transaction.
         remain_custom_asset: remain custom amount within escrow wallet
     """
+    escrow_address = escrow_wallet['@id']
+    escrow_data = escrow_wallet['data']
+    provider_address = escrow_data['provider_address']
+    creator_address = escrow_data['creator_address']
+    remain_custom_asset: Decimal = Decimal(escrow_wallet['asset'][settings['ASSET_CODE']])
 
-    builder = Builder(address=escrow_address, network=settings['STELLAR_NETWORK'])
+    builder = Builder(address=creator_address, network=settings['STELLAR_NETWORK'])
 
     if remain_custom_asset > 0:
-        builder.append_payment_op(destination=provider_address, amount=remain_custom_asset, asset_type=settings['ASSET_CODE'], asset_issuer=settings['ISSUER'])
+        builder.append_payment_op(destination=provider_address, amount=remain_custom_asset, asset_type=settings['ASSET_CODE'], asset_issuer=settings['ISSUER'], source=escrow_address)
 
-    builder.append_account_merge_op(destination=creator_address)
+    builder.append_trust_op(settings['ISSUER'], settings['ASSET_CODE'], limit=0, source=escrow_address)
+
+    for name in escrow_data.keys():
+        builder.append_manage_data_op(data_name=name, data_value=None, source=escrow_address)
+    # await remove_data_from_builder(builder, escrow_address, escrow_data.keys())
+    builder.append_account_merge_op(destination=creator_address, source=escrow_address)
 
     try:
         xdr = builder.gen_xdr()
@@ -79,4 +78,16 @@ async def build_generate_close_escrow_wallet_transaction(escrow_address: str,
 
     tx_hash = builder.te.hash_meta()
 
+
+
+
+
+
     return xdr.decode(), binascii.hexlify(tx_hash).decode()
+
+# async def payment_remainting_asset_to_creator(builder)
+
+async def remove_data_from_builder(builder: Builder, source_address: str, data: list):
+    pass
+
+
