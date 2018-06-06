550f461577441c66df52d7a157d2957f0ce09a1f
SIwakorn <siwakorn@proteus-tech.com>
2018-06-06 13:40:19 +0700

    use transaction_source_address when close escrow wallet

--- token_platform/escrow/post_close_escrow_wallet.py
+++ token_platform/escrow/post_close_escrow_wallet.py
@@ -11,8 +11,10 @@ import binascii
 async def post_close_escrow_wallet_from_request(request: web.Request) -> web.Response:
     """ AIOHTTP Request create close escrow wallet xdr """
     escrow_address = request.match_info['escrow_address']
+    body = await request.json()
+    transaction_source_address = body['transaction_source_address']
 
-    result = await generate_merge_transaction(escrow_address)
+    result = await generate_merge_transaction(transaction_source_address, escrow_address)
     result['@id'] = reverse('close-escrow-wallet', escrow_address=escrow_address)
 
     return web.json_response(result)
