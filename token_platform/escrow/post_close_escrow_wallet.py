075b09f1c68e60a5eb3c4dd5439660068fde87d7
sem <pumidon@proteus-tech.com>
2018-06-12 12:08:12 +0700

    Delete unused code and sort import

--- token_platform/escrow/post_close_escrow_wallet.py
+++ token_platform/escrow/post_close_escrow_wallet.py
@@ -1,20 +1,15 @@
 from aiohttp import web
-from stellar_base.builder import Builder
-from conf import settings
-from typing import Tuple, Any, Dict
-from decimal import Decimal
+
 from router import reverse
 from transaction.generate_merge_transaction import generate_merge_transaction
 
-import binascii
 
 async def post_close_escrow_wallet_from_request(request: web.Request) -> web.Response:
     """ AIOHTTP Request create close escrow wallet xdr """
     escrow_address = request.match_info['escrow_address']
 
     result = await generate_merge_transaction(escrow_address)
-    result['@id'] = reverse('close-escrow-wallet', escrow_address=escrow_address)
+    result['@id'] = reverse('close-escrow-wallet',
+                            escrow_address=escrow_address)
 
     return web.json_response(result)
-
-
