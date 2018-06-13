a23c51fb2bc4270ba6132091c24fbf7611262f93
Tle Ekkul <aryuth.ekkul@proteus-tech.com>
2018-06-13 19:30:00 +0700

    make config need to be explicit in the environment

--- token_platform/conf.py
+++ token_platform/conf.py
@@ -3,6 +3,6 @@ import os
 settings = dict()
 
 settings['HOST'] = os.getenv('HOST', '')
-settings['ASSET_CODE'] = os.getenv('ASSET_CODE', 'HTKN')
-settings['ISSUER'] = os.getenv('ISSUER', 'GDSB3JZDYKLYKWZ6NXDPPGPCYJ32ISMTZ2LVF5PYQGY4B4FGNIU2M5BJ')
-settings['STELLAR_NETWORK'] = os.getenv('STELLAR_NETWORK', 'TESTNET')
+settings['ASSET_CODE'] = os.environ['ASSET_CODE']
+settings['ISSUER'] = os.environ['ISSUER']
+settings['STELLAR_NETWORK'] = os.environ['STELLAR_NETWORK']
