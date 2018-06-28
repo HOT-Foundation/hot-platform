5c80dcec1a7a345b148a7412788d577a522379d7
sem <pumidon@proteus-tech.com>
2018-06-28 20:17:03 +0700

    Struture directories and files

--- token_platform/server.py
+++ token_platform/server.py
@@ -6,15 +6,13 @@ from aiohttp import ClientSession, web
 from aiohttp.test_utils import AioHTTPTestCase, unittest_run_loop
 
 from controller import handle
-from log_conf import log_setting
-from log_middleware import handle_log
-from exception_middleware import handle_error
+from middlewares import exception, log, log_conf
 from router import generate_routes
 
 
 async def init_app():
     """Initialize the application server."""
-    app = web.Application(middlewares=[handle_log, handle_error])
+    app = web.Application(middlewares=[log.handle_log, exception.handle_error])
     app.add_routes(generate_routes())
     return app
 
@@ -22,5 +20,5 @@ async def init_app():
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
     app = loop.run_until_complete(init_app())
-    config.dictConfig(log_setting)
+    config.dictConfig(log_conf.log_setting)
     web.run_app(app, port=80)
